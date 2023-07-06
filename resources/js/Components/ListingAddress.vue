08e58d66a21408cd4749225b786f48dfe59b9dc1
Angad Gupta <i_am_angad@hotmail.com>
2023-07-06 12:13:59 -0400

    first push

---
+++ resources/js/Components/ListingAddress.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        {{ listing.street }}
+    </div>
+</template>
+<script setup>
+
+    defineProps({
+        listing: Array,
+    })
+</script>
+
---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Show</div>
+
+        {{ listing.street }}
+
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+    defineProps({
+        listing: Object,
+    })
+</script>
+
