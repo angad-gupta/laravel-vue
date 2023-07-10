2f04b4c47dd98f47021126af2f6c37f2340301fa
Angad Gupta <i_am_angad@hotmail.com>
2023-07-10 17:32:46 -0400

    auth

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,5 @@
 <template>
-  
- <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+  <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
     <div class="container mx-auto">
       <nav class="p-4 flex items-center justify-between">
         <div class="text-lg font-medium">
@@ -9,29 +8,38 @@
         <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
           <Link :href="route('listing.index')">LaraZillow</Link>
         </div>
-        <div>
+        <div v-if="user" class="flex items-center gap-4">
+          <div class="text-sm text-gray-500">{{ user.name }}</div>
           <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
+          <Link :href="route('logout')"  method="DELETE" as="button">Logout</Link>
+        </div>
+        <div v-else class="flex items-center gap-2">
+          <Link :href="route('user-account.create')">Register</Link>
+          <Link :href="route('login')">Sign-In</Link>
         </div>
       </nav>
-      <slot>Default</slot>
     </div>
   </header>
   
-
-    <!-- <div v-if="flashSuccess" class="success">
+  
+  <main class="container mx-auto p-4 w-full">
+    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
       {{ flashSuccess }}
-    </div> -->
-  </template>
-  
-  <script setup>
-  import { computed } from 'vue'
-  import { Link, usePage } from '@inertiajs/vue3'
-  
+    </div>
+    <slot>Default</slot>
+  </main>
+</template>
 
-  
-  const page = usePage()
-  const flashSuccess = computed(
-    () => page.props.value.flash.success,
-  )
-  </script>
-  
+<script setup>
+import { computed } from 'vue'
+import { Link, usePage } from '@inertiajs/vue3'
+
+const page = usePage()
+
+const flashSuccess = computed(
+  () => page.props.flash.success,
+)
+const user = computed(
+  () => page.props.user,
+)
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
-import MainLayout from './Layouts/MainLayout.vue'
+import MainLayout from '@/Layouts/MainLayout.vue'
 import { ZiggyVue } from 'ziggy'
 import { InertiaProgress } from '@inertiajs/progress'
 import '../css/app.css'
@@ -13,13 +13,16 @@ InertiaProgress.init({
 })
 
 createInertiaApp({
-  resolve: async(name) => {
+  resolve: async (name) => {
     const pages = import.meta.glob('./Pages/**/*.vue')
-    const page =  await pages[`./Pages/${name}.vue`]()
-    page.default.layout = page.default.layout || MainLayout
-    return page
 
+    const page = await pages[`./Pages/${name}.vue`]()
+    page.default.layout = page.default.layout || MainLayout
+
+    return page
   },
+  // function () {}
+  // {el: xxx, App: xxx, props: xxx}
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
